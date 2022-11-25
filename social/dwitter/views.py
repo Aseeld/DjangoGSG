6f3457155ede2aea74b1e17184303c1bc250d1cc
aseeld <aseel.dumaidi@gmail.com>
2022-11-25 20:39:29 +0200

    Dwitter social

---
+++ social/dwitter/views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render,redirect
+from .forms import DweetForm
+from .models import Profile,Dweet 
+from django.http import HttpResponseRedirect
+# def dashboard(request):
+#     form = DweetForm()
+#     if request.method == 'POST':
+#         form = DweetForm(request.POST)
+#         if form.is_valid():            
+#             dweet=Dweet(body=form.cleaned_data["body"],
+#             user=request.user
+#             )
+#         dweet.save()
+#     form = DweetForm()
+#     return render(request, "dwitter/dashboard.html", {"form": form})
+def dashboard(request):
+    form = DweetForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            dweet = form.save(commit=False)
+            dweet.user = request.user
+            dweet.save()
+            return redirect("dwitter:dashboard")
+        #return redirect(request.path_info)
+        # return HttpResponseRedirect(request.path_info)
+    # else:
+    #     form = DweetForm()
+    return render(request, "dwitter/dashboard.html", {"form": form})
+
+def profile_list(request):
+    profiles = Profile.objects.exclude(user=request.user)
+    return render(request, "dwitter/profile_list.html", {"profiles": profiles})
+
+def profile(request, pk):
+    if not hasattr(request.user, 'profile'):
+        missing_profile = Profile(user=request.user)
+        missing_profile.save()
+
+    profile = Profile.objects.get(pk=pk)
+    if request.method == "POST":
+        current_user_profile = request.user.profile
+        data = request.POST
+        action = data.get("follow")
+        if action == "follow":
+            current_user_profile.follows.add(profile)
+        elif action == "unfollow":
+            current_user_profile.follows.remove(profile)
+        current_user_profile.save()
+    return render(request, "dwitter/profile.html", {"profile": profile})
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    
+    form = D()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+            author=form.cleaned_data["author"],
+            body=form.cleaned_data["body"],
+            post=post
+            )
+        comment.save()
+
+
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+        "form": form,
+        }
+    return render(request, "blog_detail.html", context)
